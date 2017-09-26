bc37bdece0a6b9bc06dc3147c79c5087409cdbad
Shana L Moore <shana.lavina.moore@gmail.com>
2017-09-26 15:04:58 -0700

    first commit

---
+++ body-mass-index-lab/bmi.c
@@ -0,0 +1,55 @@
+//
+//  body-mass-index-lab.c
+//  body-mass-index-lab
+//
+//  Created by Shana Moore on 9/26/17.
+//  Copyright © 2017 Shana Moore. All rights reserved.
+/*  Construct a C program which includes a function, calculateBMI(), that prompts the user to input a weight, in pounds, and height, in inchese, and then calculates and displays the body mass index. */
+
+#include <stdio.h>
+
+int calculateBMI(void); // function prototype
+
+int main(void) {
+    // Declare variables with appropriate data types
+    int i = 0;
+    FILE *fp = NULL;
+    
+    // Output to file
+    fp = fopen("csis.txt", "w");
+    for (i = 1; i <= 4; ++i){
+        calculateBMI();
+    }
+    fclose(fp);
+    return 0;
+}
+
+int calculateBMI(void){
+    // Declare variables with appropriate data types
+    double BMI = 0, weightInPounds = 0, heightInInches = 0;
+
+    // Get height and weight from user
+    printf("Enter your weight (in pounds): ");
+    scanf("%lf", &weightInPounds);
+    
+    printf("Enter your height (in inches): ");
+    scanf("%lf", &heightInInches);
+
+    // Compute BMI per user's input
+    BMI = (weightInPounds * 703) / (heightInInches * heightInInches);
+    
+    // Display output of user's BMI value and classification
+    if(BMI >= 30.0){
+        printf("Your BMI is %.1f and you are obese.\n", BMI);
+    }
+    else if (BMI >= 25.0){
+        printf("Your BMI is %.1f and you are overweight.\n", BMI);
+    }
+    else if (BMI >= 18.5){
+        printf("Your BMI is %.1f and you are normal.\n", BMI);
+    }
+    else {
+        printf("Your BMI is %.1f and you are underweight.\n", BMI);
+    }
+    return 0;
+}
